be86c28e80d45a6b5843639e6caf14a336c9e7e9
dangminhngo <dangminhngo.dev@gmail.com>
2024-06-04 17:44:05 +0700

    fix: update ex01_01

--- files/01/ex01_01.cpp
+++ files/01/ex01_01.cpp
@@ -1,14 +1,7 @@
 #include <iostream>
 
-void print(int x);
-
 int main() {
   int answer{42};
   std::cout << "The answer to life, the universe, and everything is " << answer
             << std::endl;
-  print(answer);
-}
-
-void print(int x) {
-  std::cout << x << std::endl;
 }
